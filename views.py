b84a11402fa69802798e6b188bff506592cfff81
vubon <vubon.roy@gmail.com>
2020-05-09 18:06:46 +0600

    - Introduce SQLite3 DB - requirements.txt file updated - Create DB model - Create object, update functions

--- views.py
+++ views.py
@@ -4,6 +4,8 @@ import aiohttp
 import aiohttp_jinja2
 from aiohttp import web
 
+from db import create_record, fetch_last_data, update_record
+
 log = logging.getLogger(__name__)
 
 
@@ -15,7 +17,8 @@ async def fetch(session, url):
 async def summary():
     """
     {"cases":229881,"deaths":9377,"recovered":86254}
-    {"country":"China","cases":80928,"todayCases":34,"deaths":3245,"todayDeaths":8,"recovered":70420,"active":7263,"critical":2274,"casesPerOneMillion":56}
+    {"country":"China","cases":80928,"todayCases":34,"deaths":3245,"todayDeaths":8,"recovered":70420,"active":7263,
+    "critical":2274,"casesPerOneMillion":56}
     :return:
     """
     async with aiohttp.ClientSession() as session:
@@ -46,6 +49,17 @@ async def index(request):
         bd['state'] = 'Bangladesh'
         world = await summary()
         world['state'] = 'global'
+        create = {"today_deaths": bd.get("todayDeaths", 0), "today_affected": bd.get("todayCases", 0)}
+        # create = {"today_deaths": 9, "today_affected": 1000}
+        fetch_data = await fetch_last_data()
+
+        if fetch_data:
+            fetch_dict = fetch_data.to_dict()
+            if fetch_dict["death"] != create["today_deaths"] and fetch_dict["affected"] != create['today_affected']:
+                await update_record(fetch_data, create)
+        else:
+            await create_record(**create)
+
         # all_countries = await top_deaths_country()
         # async for country in all_countries:
         #
